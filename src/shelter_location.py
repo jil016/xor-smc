e7294ea56cb7d59b2f61acfcc8f2fb6780f51c34
Jinzhao Li <jinzhao@Jinzhaos-MacBook-Pro.local>
2023-07-27 19:43:03 -0400

    graph init

--- src/shelter_location.py
+++ src/shelter_location.py
@@ -1,30 +1,53 @@
 import numpy as np
 from sympy import *
 
+# np.random.seed(10086)
+
 
 class Graph:
-    def __init__(self, N, mode='empty'):
+    def __init__(self, N, mode='empty', allow_loop='true'):
         self.N = N
         self.M = 0
-        self.adj = [[] for i in range(N)]
-        self.Adj = [[0 for j in range(N)] for i in range(N)]
+        
+        if(mode == 'empty'):
+            self.Adj = [[0 for j in range(self.N)] for i in range(self.N)]
+        elif(allow_loop == 'true'):
+            self.normalInit(mode)
+        else:
+            self.loopFreeInit(mode)
 
+    def normalInit(self, mode):
         if(mode == 'full'):
-            for i in range(N):
-                for j in range(N):
-                    if (j != i):
-                        self.adj[i].append(j)
-                        self.Adj[i][j] = 1
-                        self.M += 1
+            self.Adj = [[1 for j in range(self.N)] for i in range(self.N)]
+            for i in range(self.N):
+                self.Adj[i][i] = 0 # no self loop
 
+            self.M = np.sum(self.Adj)
+        
+        if(mode == 'random'):
+            self.Adj = np.random.randint(low=0, high=2, 
+                                         size=(self.N,self.N),
+                                         dtype=int).tolist()
+            self.M = np.sum(self.Adj)
 
-    def randomInit(self):
-        pass
- 
+    def loopFreeInit(self, mode):
+        # this is fully at random
+        if(mode == 'full'):
+            self.Adj = [[1 for j in range(self.N)] for i in range(self.N)]
+            for i in range(self.N):
+                for j in range(i + 1):
+                    self.Adj[i][j] = 0 # no self loop
+
+            self.M = np.sum(self.Adj)
+        if(mode == 'random'):
+            n_hierarchy = np.random.randint(low=0, high=self.N)
+            
+        
+        return
+
     def addEdge(self, u, v):
         # Add v to u’s list.
         self.M += 1
-        self.adj[u].append(v)
         self.Adj[u][v] = 1
         return
  
@@ -57,8 +80,8 @@ class Graph:
             else: 
                 # Recur for all the vertices
                 # adjacent to current vertex
-                for v in self.adj[u]:
-                    if (not visited[v]):
+                for v, value in enumerate(self.Adj[u]):
+                    if (value == 1) & (not visited[v]):
                         queue.append(v)
                         visited_que.append([j for j in visited])
 
@@ -67,29 +90,30 @@ class Graph:
 
 
 def pathIdentifier(graph: Graph, flow: list, hasloop = 'false'):
+    # return a logic formula that determine whether it is a valid path
     return
-
 
 
 def graphTester():
     # Create a graph given in the
     # above diagram
-    g = Graph(4, )
-    g.addEdge(0, 1)
-    g.addEdge(1, 2)
-    g.addEdge(2, 3)
-    g.addEdge(3, 1)
+    g = Graph(4, 'full', 'false')
+    # g.addEdge(0, 1)
+    # g.addEdge(1, 2)
+    # g.addEdge(2, 3)
+    # g.addEdge(3, 1)
     # g.addEdge(1, 4)
     s = 0
     d = 3
      
     # Function call
-    print(g.countLoopFreePaths(s, d))
-    print(g.countLoopFreePaths2(s, d))
+    print(g.countLoopFreePathsDFS(s, d))
     print(g.N, g.M)
+    print(g.Adj)
     return
 
 # Driver Code
 if __name__ == '__main__':
-    
-    x_e = [Symbol(f'x{i}_{j}') for i in range(n)]
+    M = 10
+    x_e = [Symbol(f'x_e{i}') for i in range(M)]
+    graphTester()
