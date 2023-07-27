86e743d7ec4a7053773adc9189741560a1fdee18
Jinzhao <li4255@purdue.edu>
2023-07-27 18:36:27 -0400

    shelter

--- src/shelter_location.py
+++ src/shelter_location.py
@@ -3,7 +3,6 @@ from sympy import *
 
 
 class Graph:
- 
     def __init__(self, N, mode='empty'):
         self.N = N
         self.M = 0
@@ -17,81 +16,80 @@ class Graph:
                         self.adj[i].append(j)
                         self.Adj[i][j] = 1
                         self.M += 1
+
+
+    def randomInit(self):
+        pass
  
     def addEdge(self, u, v):
         # Add v to u’s list.
         self.M += 1
         self.adj[u].append(v)
         self.Adj[u][v] = 1
- 
-    # Returns count of paths from 's' to 'd'
-
- 
-    # A recursive function to print all paths
-    # from 'u' to 'd'. visited[] keeps track
-    # of vertices in current path. path[]
-    # stores actual vertices and path_index
-    # is current index in path[]
-    def countPathsUtil(self, u, d,
-                       visited, pathCount):
-        visited[u] = True
- 
-        # If current vertex is same as
-        # destination, then increment count
-        if (u == d):
-            pathCount[0] += 1
- 
-        # If current vertex is not destination
-        else: 
-            # Recur for all the vertices
-            # adjacent to current vertex
-            i = 0
-            while i < len(self.adj[u]):
-                if (not visited[self.adj[u][i]]):
-                    self.countPathsUtil(self.adj[u][i], d,
-                                        visited, pathCount)
-                i += 1
- 
-        visited[u] = False
         return
-    
+ 
+   
     def countEdgeOncePaths(self, ):
         # Count paths that pass each edge at most once.
 
         return
     
-    def countLoopFreePaths(self, s, d):
+    def countLoopFreePathsDFS(self, s, d):
         # Mark all the vertices
         # as not visited
         visited = [False] * self.N
  
         # Call the recursive helper
         # function to print all paths
-        pathCount = [0]
-        self.countPathsUtil(s, d, visited, pathCount)
-        return pathCount[0]
+        pathCount = 0
+        queue = [s]
+        visited_que = [visited]
+        while queue:
+            u = queue.pop()
+            visited = visited_que.pop()
+
+            visited[u] = True
+            # If current vertex is same as
+            # destination, then increment count
+            if (u == d):
+                pathCount += 1
+            # If current vertex is not destination
+            else: 
+                # Recur for all the vertices
+                # adjacent to current vertex
+                for v in self.adj[u]:
+                    if (not visited[v]):
+                        queue.append(v)
+                        visited_que.append([j for j in visited])
+
+        return pathCount
 
 
 
-def pathIdentifier(flow: list, hasloop = 'false'):
+def pathIdentifier(graph: Graph, flow: list, hasloop = 'false'):
     return
 
 
 
-# Driver Code
-if __name__ == '__main__':
- 
+def graphTester():
     # Create a graph given in the
     # above diagram
-    g = Graph(4, 'full')
-    # g.addEdge(0, 1)
-    # g.addEdge(1, 2)
-    # g.addEdge(2, 3)
-    # g.addEdge(3, 1)
+    g = Graph(4, )
+    g.addEdge(0, 1)
+    g.addEdge(1, 2)
+    g.addEdge(2, 3)
+    g.addEdge(3, 1)
     # g.addEdge(1, 4)
     s = 0
     d = 3
      
     # Function call
     print(g.countLoopFreePaths(s, d))
+    print(g.countLoopFreePaths2(s, d))
     print(g.N, g.M)
+    return
+
+# Driver Code
+if __name__ == '__main__':
+    
+    x_e = [Symbol(f'x{i}_{j}') for i in range(n)]
