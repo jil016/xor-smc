72f41d0be9b0781295dfee6d8793fa870ba59d6a
Jinzhao <li4255@purdue.edu>
2023-07-28 04:45:21 -0400

    supply chain started

---
+++ src/xor_smt.py
@@ -0,0 +1,18 @@
+import numpy as np
+import shelter_location as sl
+import supply_chain as sc
+from sympy import *
+
+
+def add_XOR_constraints():
+    pass
+
+
+def export_CNF():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    pass
