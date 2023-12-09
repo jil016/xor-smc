8e93f8f3d62d4c039313ac3859c4cb929ae48d32
Jinzhao <li4255@purdue.edu>
2023-12-09 10:29:13 -0500

    baseline

--- baselines/supply_chain/sampler/bayes_net_sampler/bayesian.py
+++ baselines/supply_chain/sampler/bayes_net_sampler/bayesian.py
@@ -1,22 +1,18 @@
 import numpy as np
 
-
 class BayesianNetwork:
     def __init__(self, network_folder):
         self.folder = network_folder
         self.initialize_from_file()
 
-
     def initialize_from_file(self):
         pass
 
 
-
-
 def Bayesian_Sampling(filename, n_samples):
-
     bnet = BayesianNetwork(filename)
 
     pass
+
 
 if __name__ == "__main__":
